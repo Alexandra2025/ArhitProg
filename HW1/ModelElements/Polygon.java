6ec885797d0940d2314c09cc846460f08e61fd11
Alexandra2025 <turkova2025@gmail.com>
2024-02-16 19:56:18 +0800

    Add HW1.

---
+++ HW1/ModelElements/Polygon.java
@@ -0,0 +1,12 @@
+package HW1.ModelElements;
+
+import HW1.stuff.Point3D;
+
+import java.util.List;
+
+public class Polygon {
+    public List<Point3D> points;
+    public Polygon(List<Point3D> points){
+        this.points = points;
+    }
+}
---
+++ HW1/stuff/Point3D.java
@@ -0,0 +1,5 @@
+package HW1.stuff;
+
+public class Point3D {
+    
+}
